3a5d1d3cfa163d9180fcb1d944d5e80397f61e4a
craymorgana <aaron.morgan.7@outloook.com>
2022-09-28 20:43:53 -0600

    functionality of the password generator is complete

--- Develop/script.js
+++ Develop/script.js
@@ -14,31 +14,35 @@ function generatePassword(){
   var LOWERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   var SPECIALS = ['!','#','$','%','^','&','*', '(', ')', '_', '-', '=', '+', '[', ']', '{', '}', '\\', '|', '\,' ,'.','/','?'];
   var NUMBERS = ['0','1','2','3','4','5','6','7','8','9']; 
+  12
+  // Var Declare
+  var passwordChoice = [];
 
   
   var length = window.prompt("How many characters do you want?");
   //Re-prompt - If the user has not entered a valid number //
-  while (length < 8 || length > 128){
-    window.alert("Please enter a number between 8-128");
-    length = window.prompt("How many characters do you want?")
-  }
+    while (length < 8 || length > 128){
+      window.alert("Please enter a number between 8-128");
+      length = window.prompt("How many characters do you want?")
+    }
   
-  var lowerBool = window.confirm("Do you want lowercase characters?");
-  var upperBool = window.confirm("Do you want uppercase characters?");
-  var specialBool = window.confirm("Do you want special characters?");
+   var lowerBool = window.confirm("Do you want lowercase characters?");
+   var upperBool = window.confirm("Do you want uppercase characters?");
+   var specialBool = window.confirm("Do you want special characters?");
   
 
-  // combine arrays for each confirmation
+// combine arrays for each confirmation
   switch(true){
     // All true
     case (lowerBool && upperBool && specialBool):
       passwordChoice = LOWERS.concat(UPPERS, SPECIALS, NUMBERS);
     break;
+    
     // true lower, true upper, false special
     case (lowerBool && upperBool && !specialBool):
       passwordChoice = LOWERS.concat(UPPERS, NUMBERS);
       break;
-    
+  
     // true lower, false upper, true special
     case (lowerBool && !upperBool && specialBool):
       passwordChoice = LOWERS.concat(SPECIALS, NUMBERS);
@@ -71,15 +75,14 @@ function generatePassword(){
   } 
 
   
-  
-  
   // generate password
+  var newpassword = [];
   for (var i = 0; i < length; i++){
-    var RANDOM = Math.floor(Math.random(0, passwordChoice.length))
-    var newpassword = [RANDOM];
-    password = newpassword.push(newpassword);
-    }
- 
+    var RANDOM = Math.floor((Math.random() * passwordChoice.length))
+    newpassword.push(passwordChoice[RANDOM]);
+  }
+  console.log('newpassword :>> ', newpassword);
+  password = newpassword.join("");
   return password;
 }
 
