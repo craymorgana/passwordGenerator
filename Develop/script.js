e3e554408bf54ac274f77c4e3d40187c8d7a602e
craymorgana <aaron.morgan.7@outloook.com>
2022-10-01 12:47:21 -0600

    numbers are asked if they want to be in the password. created catches for incorrect password length

--- Develop/script.js
+++ Develop/script.js
@@ -14,63 +14,98 @@ function generatePassword(){
   var LOWERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   var SPECIALS = ['!','#','$','%','^','&','*', '(', ')', '_', '-', '=', '+', '[', ']', '{', '}', '\\', '|', '\,' ,'.','/','?'];
   var NUMBERS = ['0','1','2','3','4','5','6','7','8','9']; 
-  12
+
   // Var Declare
   var passwordChoice = [];
 
   
-  var length = window.prompt("How many characters do you want?");
-  //Re-prompt - If the user has not entered a valid number //
-    while (length < 8 || length > 128){
-      window.alert("Please enter a number between 8-128");
-      length = window.prompt("How many characters do you want?")
-    }
+  var length = prompt("How many characters do you want?");
+  length = parseInt(length);
   
+  //Re-prompt - If the user has not entered a valid number //
+  if (length < 8 || length > 128){
+    window.alert("Please enter a number between 8-128");
+    length = window.prompt("How many characters do you want?");
+    length = parseInt(length);
+  }
+
+  while(isNaN(length) || length < 8 || length > 128){
+      window.alert("Please enter a number between 8-128");
+      length = window.prompt("How many characters do you want?");
+      length = parseInt(length);
+  }
+
    var lowerBool = window.confirm("Do you want lowercase characters?");
    var upperBool = window.confirm("Do you want uppercase characters?");
    var specialBool = window.confirm("Do you want special characters?");
+   var numberBool = window.confirm("Do you want numbers?");
   
 
 // combine arrays for each confirmation
   switch(true){
-    // All true
-    case (lowerBool && upperBool && specialBool):
-      passwordChoice = LOWERS.concat(UPPERS, SPECIALS, NUMBERS);
-    break;
-    
-    // true lower, true upper, false special
-    case (lowerBool && upperBool && !specialBool):
-      passwordChoice = LOWERS.concat(UPPERS, NUMBERS);
-      break;
-  
-    // true lower, false upper, true special
-    case (lowerBool && !upperBool && specialBool):
-      passwordChoice = LOWERS.concat(SPECIALS, NUMBERS);
-      break;
-
-    // true lower, false upper, false special
-    case (lowerBool && !upperBool && !specialBool):
-      passwordChoice = LOWERS.concat(NUMBERS);
-      break;
-
-    // false lower, true upper, true special
-    case (!lowerBool && upperBool && specialBool):
-      passwordChoice = UPPERS.concat(SPECIALS, NUMBERS);
-      break;
-
-    // false lower, true upper, false special
-    case (!lowerBool && upperBool && !specialBool):
-      passwordChoice = UPPERS.concat(NUMBERS);
-      break;
-
-    // false lower, false upper, true special
-    case (!lowerBool && !upperBool && specialBool):
-      passwordChoice = SPECIALS.concat(NUMBERS);
-      break;
-
     // all false
-    case (!lowerBool && !upperBool && !specialBool):
+    case (!lowerBool && !upperBool && !specialBool && !numberBool):
       passwordChoice = NUMBERS;
+    break;
+    // true lower, false upper, false special, false number
+    case (lowerBool && !upperBool && !specialBool && !numberBool):
+      passwordChoice = LOWERS;
+    break;
+    // false lower, true upper, false special, false number
+    case (!lowerBool && upperBool && !specialBool && !NUMBERS):
+      passwordChoice = UPPERS;
+    break;
+    //false lower, false upper, true special, false number
+    case (!lowerBool && !upperBool && specialBool && !numberBool):
+      passwordChoice = SPECIALS;
+    break;
+        //false lower, false upper, false special, true number
+    case (!lowerBool && !upperBool && !specialBool && numberBool):
+      passwordChoice = NUMBERS;
+    break;
+      //true lower, true upper, false special, false number
+    case (lowerBool && upperBool && !specialBool && !numberBool):
+      passwordChoice = LOWERS.concat(UPPERS);
+    break;
+    //true lower, false upper, true special, false number
+    case (lowerBool && !upperBool && specialBool && !numberBool):
+      passwordChoice = LOWERS.concat(SPECIALS);
+    break;
+        //true lower, false upper, false special, true number
+    case (lowerBool && !upperBool && !specialBool && numberBool):
+      passwordChoice = LOWERS.concat(NUMBERS);
+    break;
+        //false lower, true upper, true special, false number
+    case (!lowerBool && upperBool && specialBool && !numberBool):
+      passwordChoice = UPPERS.concat(SPECIALS);
+    break;
+    //false lower, true upper, false special, true number
+    case (!lowerBool && upperBool && !specialBool && numberBool):
+      passwordChoice = LOWERS.concat(NUMBERS);
+    break;
+    //false lower, false upper, true special, true number   
+    case (!lowerBool && !upperBool && specialBool && numberBool):
+      passwordChoice = SPECIALS.concat(NUMBERS);
+    break;
+    //true lower, true upper, true special, false number
+    case (lowerBool && upperBool && specialBool && !numberBool):
+      passwordChoice = LOWERS.concat(UPPERS, SPECIALS);
+    break;    
+    //true lower, true upper, false special, true number
+    case (lowerBool && upperBool && !specialBool && numberBool):
+      passwordChoice = LOWERS.concat(uppers, NUMBERS);
+    break;
+        //true lower, false upper, true special, true number
+    case (lowerBool && !upperBool && specialBool && numberBool):
+      passwordChoice = LOWERS.concat(SPECIALS, NUMBERS);
+    break;
+        //false lower, true upper, true special, true number
+    case (!lowerBool && upperBool && specialBool && numberBool):
+      passwordChoice = UPPERS.concat(SPECIALS, NUMBERS);
+    break;
+        //all true
+    case (lowerBool && upperBool && specialBool && numberBool):
+      passwordChoice = LOWERS.concat(UPPERS, SPECIALS, NUMBERS);
     break;
   } 
 
